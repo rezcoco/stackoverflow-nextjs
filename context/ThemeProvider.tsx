92c891bae6073e3f4c5c89507f9d901f76a2d421
rezcoco <rezaainulzaki23@gmail.com>
2023-10-28 12:29:49 +0700

    add theme context

---
+++ context/ThemeProvider.tsx
@@ -0,0 +1,41 @@
+"use client";
+import React from "react";
+
+type Theme = "light" | "dark";
+
+type ThemeContextType = {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+};
+
+const ThemeContext = React.createContext<ThemeContextType | undefined>(
+  undefined
+);
+
+export const ThemeProvider = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = React.useState<Theme>("light");
+
+  React.useEffect(() => {
+    if (theme === "light") {
+      setTheme("dark");
+      document.documentElement.classList.add("dark");
+    } else {
+      setTheme("light");
+      document.documentElement.classList.add("light");
+    }
+  }, [theme]);
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => {
+  const context = React.useContext(ThemeContext);
+  if (!context)
+    throw new Error("useTheme must be used within the ThemeProvider");
+
+  return context;
+};
