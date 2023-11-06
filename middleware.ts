f7ca71d139c1506cacc714a5d21cecd78d609f01
rezcoco <rezaainulzaki23@gmail.com>
2023-11-06 22:42:52 +0700

    fixup! set middleware to accept webhook

--- middleware.ts
+++ middleware.ts
@@ -2,6 +2,7 @@ import { authMiddleware } from "@clerk/nextjs";
 
 export default authMiddleware({
     publicRoutes: ["/", "/api/webhook"],
+    ignoredRoutes: ["/api/webhook", "/api/chatgpt"]
 });
 
 export const config = {
