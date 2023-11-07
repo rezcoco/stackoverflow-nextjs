ccd9fe79089f46d793f93c23ccd3b49a819feb1d
rezcoco <rezaainulzaki23@gmail.com>
2023-11-07 14:37:56 +0700

    fixup! fixup! testing

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -4,12 +4,6 @@ let isConnected = false
 export async function connectToDatabase() {
     mongoose.set("strictQuery", true)
 
-    console.log(process.env.WEBHOOK_SECRET)
-
-    if (!process.env.WEBHOOK_SECRET) {
-        return console.log("Webhook secret key not provided")
-    }
-
     if (isConnected) {
         return console.log("Mongodb already connected")
     }
--- middleware.ts
+++ middleware.ts
@@ -3,7 +3,6 @@ import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
     publicRoutes: ["/", "/api/webhook"],
     ignoredRoutes: ["/api/webhook", "/api/chatgpt"],
-    debug: true
 });
 
 export const config = {
